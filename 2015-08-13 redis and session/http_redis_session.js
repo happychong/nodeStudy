d8ebce7829102eae754456917e49479e96550a65
happychong <1173290465@qq.com>
2015-08-14 13:44:14 +0800

    redis and session

---
+++ 2015-08-13 redis and session/http_redis_session.js
@@ -0,0 +1,20 @@
+/**
+ * Created by v_songxiaodan on 2015/8/13.
+ */
+var http = require('http');
+var RedisSession = require('./Redis Session');
+var url = require('url');
+
+var redisSession = new RedisSession({});
+
+http.createServer(function (req, res) {
+    var urlObj = url.parse(req.url, true);
+    if ('/favicon.ico' === urlObj.pathname) {
+        res.writeHead(404);
+        res.end(http.STATUS_CODES[404]);
+    } else {
+        redisSession.wrap(req, res, function (req, res) {
+            res.end('OK');
+        });
+    }
+}).listen(8888);
