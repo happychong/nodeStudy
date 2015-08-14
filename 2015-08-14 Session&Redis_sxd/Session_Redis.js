5d96e871f773dd743679e8c9cd960405063fa613
happychong <1173290465@qq.com>
2015-08-14 21:51:30 +0800

    session sxd

--- 2015-08-14 Session&Redis_sxd/Session_Redis.js
+++ 2015-08-14 Session&Redis_sxd/Session_Redis.js
@@ -28,7 +28,43 @@ function SessionRedis(options) {
     this._redisClient = redis.createClient(this._redisPort, this._redisHost);
 }
 SessionRedis.prototype.wrap = function (req, res, callback) {
+    if (req.headers.cookie) {
+        // cookie 存在
+        var cookieObj = cookie.parse(req.headers.cookie);
+        var self = this;
+        var sessId = cookieObj['__create-_-time__'];
+        this._redisClient.hgetall(cookieObj['__create-_-time__'], function (err, ret) {
+            err && console.log(err);
+            console.log('Ret', ret);
+            if (ret) {
+                // redis中 有数据，找到了session信息，继续延期
+                res.write('OK!请继续！已为您设置延期');
+                self._redisClient.expire(sessId, EXPTIME);
+                callback && callback.call(this);
+            } else {
+                // redis中 没数据，过期了，重新分配session
+                self.newSession(req, res);
+                res.write('过期了，重新为您分配session！');
+                callback && callback.call(this);
+            }
+        });
+    } else {
+        // cookie 不存在
+        this.newSession(req, res);
+        callback && callback.call(this);
+    }
+};
+SessionRedis.prototype.newSession = function (req, res) {
+    var sessId = uuid.v4();
+    res.setHeader('set-Cookie', '__create-_-time__=' + sessId);
+    this._redisClient.hset(sessId, '__create-_-time__', new Date().getTime(), function () {
+        console.log('redis set Ok!')
+    });
+    this._redisClient.expire(sessId, EXPTIME);
+};
 
-    var cookieObj = cookie.parse(req.headers.cookie);
-}
+
+
+
+
 module.exports = SessionRedis;
--- 2015-08-14 Session&Redis_sxd/server.js
+++ 2015-08-14 Session&Redis_sxd/server.js
@@ -8,15 +8,12 @@ var Session_Redis = require('./Session_Redis')
 var server_session = new Session_Redis();
 
 http.createServer(function (req, res) {
-    var urlObj = url.parse(req.url, true);
-    if (urlObj.pathname === '/favicon.ico') {
+    if (req.url === '/favicon.ico') {
         res.writeHead(404);
         res.end(http.STATUS_CODES[404]);
     } else {
-        console.log(req);
-        res.end('OK');
-//        server_session.wrap(req, res, function () {
-//            res.end('OK');
-//        });
+        server_session.wrap(req, res, function () {
+            res.end('END!');
+        });
     }
 }).listen(9999);
